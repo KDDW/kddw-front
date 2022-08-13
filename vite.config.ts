09fb35d94f3f9b5a0e51e46c3a56c6d5e0c890dd
Victor Ivan <victor.simoes@brmalls.com.br>
2022-08-13 17:58:01 -0300

    teste

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,17 @@
 import { defineConfig } from "vite";
+import reactRefresh from "@vitejs/plugin-react-refresh";
+import tsconfigPaths from "vite-tsconfig-paths";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), tsconfigPaths(), reactRefresh()],
 	esbuild: {
 		jsxFactory: "h",
 		jsxFragment: "Fragment",
+	},
+	build: {
+		assetsDir: "public",
 	},
 	base: "src",
 });
